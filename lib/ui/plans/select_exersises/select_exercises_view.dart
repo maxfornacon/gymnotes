ec1aae6b8e7a0eccc22982b57923b1a90d20c1f0
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2022-09-26 17:36:07 +0200

    Design select exercises

--- lib/ui/plans/select_exersises/select_exercises_view.dart
+++ lib/ui/plans/select_exersises/select_exercises_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gymnotes/ui/dumb_widgets/custom_app_bar.dart';
+import 'package:gymnotes/ui/dumb_widgets/responsive_card.dart';
+import 'package:gymnotes/ui/dumb_widgets/rounded_box.dart';
+import 'package:gymnotes/ui/dumb_widgets/rounded_button.dart';
 import 'package:gymnotes/ui/plans/select_exersises/select_exercises_viewmodel.dart';
+import 'package:gymnotes/ui/shared/styles.dart';
 import 'package:gymnotes/ui/shared/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 
@@ -13,33 +18,40 @@ class SelectExercisesView extends StatelessWidget {
       onModelReady: (model) => model.initialise(),
       builder: ((context, model, child) {
         return Scaffold(
-          appBar: AppBar(
-            title: const Text('Select Exercises'),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          appBar: getAppBar('Select Exercises', []),
+          body: ResponsiveCard(
             child: Column(
               children: [
-                vSpaceRegular,
                 model.initialized
-                    ? ListView.builder(
+                    ? ListView.separated(
                       shrinkWrap: true,
                       itemCount: model.exerciseTemplates.length,
+                      separatorBuilder: (context, index) => vSpaceSmall,
                       itemBuilder: (context, index) {
                         bool selected = model.selectedExerciseTemplates.contains(model.exerciseTemplates[index]);
-                        return CheckboxListTile(
-                          title: Text(model.exerciseTemplates[index].name),
-                          value: selected,
-                          onChanged: (bool? value) {
-                            model.selectExerciseTemplate(model.exerciseTemplates[index]);
-                          },
+                        return RoundedBox(
+                          child: CheckboxListTile(
+                            activeColor: kcPrimaryColor,
+                            title: Text(
+                              model.exerciseTemplates[index].name,
+                              style: const TextStyle(
+                                color: kcForegroundColor,
+                              ),
+                            ),
+                            value: selected,
+                            onChanged: (bool? value) {
+                              model.selectExerciseTemplate(model.exerciseTemplates[index]);
+                            },
+                          ),
                         );
                       },
                     )
                     : const Center(
                         child: CircularProgressIndicator(),
                       ),
-                ElevatedButton(
+                vSpaceRegular,
+                const Spacer(),
+                RoundedButton(
                   onPressed: () => model.selectExercisesAndGoBack(),
                   child: const Text('Add'),
                 ),
